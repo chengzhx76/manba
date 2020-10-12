b359373af7c8c2427f555c91c093552abe40d3d5
chengzhx76 <chengzhx76@qq.com>
2020-10-12 23:09:47 +0800

    add 注释

--- grpcx/publisher_etcd.go
+++ grpcx/publisher_etcd.go
@@ -24,7 +24,7 @@ func newEtcdPublisher(client *clientv3.Client, prefix string, ttl int64, timeout
 		ttl:     ttl,
 		timeout: timeout,
 		client:  client,
-		resolver: &etcdnaming.GRPCResolver{
+		resolver: &etcdnaming.GRPCResolver{ // 创建命名解析
 			Client: client,
 		},
 	}, nil
@@ -54,6 +54,8 @@ func (p *etcdPublisher) Publish(service string, meta naming.Update) error {
 
 	// etcd用于grpc命名解析与服务发现 https://blog.csdn.net/ys5773477/article/details/80216208
 	// gRPC Name Resolver 原理及实践 https://xiaomi-info.github.io/2019/12/31/grpc-custom-ns/
+	// http://morecrazy.github.io/2018/08/14/grpc-go%E5%9F%BA%E4%BA%8Eetcd%E5%AE%9E%E7%8E%B0%E6%9C%8D%E5%8A%A1%E5%8F%91%E7%8E%B0%E6%9C%BA%E5%88%B6/
 
+	// 将本服务注册添加etcd中，服务名称为 %s/%s，服务地址为 meta 里的地址
 	return p.resolver.Update(ctx, fmt.Sprintf("%s/%s", p.prefix, service), meta, clientv3.WithLease(clientv3.LeaseID(leaseResp.ID)))
 }
